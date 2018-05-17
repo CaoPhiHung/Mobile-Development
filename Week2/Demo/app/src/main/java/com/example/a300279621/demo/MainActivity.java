118cdf14b5b29752317943eb67fe6371ac74e959
CaoPhiHung <caophihung8392@gmail.com>
2018-05-17 10:26:03 -0700

    add 2 activities

---
+++ Week2/Demo/app/src/main/java/com/example/a300279621/demo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a300279621.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btnCall);
+        btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent myIntent = new Intent(MainActivity.this, Recipe.class);
+                TextView tv  = (TextView) findViewById(R.id.txtTitle);
+                myIntent.putExtra("foodTitle", tv.getText().toString());
+               startActivity(myIntent);
+           }
+        });
+        Log.d("MainActivity", "onCreate called");
+    }
+}
